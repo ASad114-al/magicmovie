ab05f0f1285da455f8684cf8fd7b0d50f1341bdf
Asad114-al <as3ad.94aa@gmail.com>
2021-01-21 10:18:33 +0100

    details

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: true }))
 const mongoose = require('mongoose');
+const { json } = require('express');
 
 mongoose.connect(process.env.dburi, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(result => {
@@ -17,9 +18,6 @@ mongoose.connect(process.env.dburi, { useNewUrlParser: true, useUnifiedTopology:
     })
     .catch(err => console.log(err))
   
-
-    // https://api.themoviedb.org/3/movie/550?api_key=4f2d4313669b932746a7cbe1b3fff187
-
     app.get('/', (req, res) => {  
 
         fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=4f2d4313669b932746a7cbe1b3fff187')
@@ -33,5 +31,77 @@ mongoose.connect(process.env.dburi, { useNewUrlParser: true, useUnifiedTopology:
     })
     .catch(err => console.log(err))     
     })
+
+
+    app.get('/details/:id' , (req, res) =>{
+    //    console.log(req.params.id);
+        fetch(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=4f2d4313669b932746a7cbe1b3fff187`)
+       
+        .then(res => res.json())
+        .then(json => {
+            // console.log(json)
+            res.render("moviesdetails", { single: json })
+        })
+      .catch(err=>console.log(err))
+      })
+
+    //   app.get('/addfavourites/:id' , (req, res) =>{
+    //     //    console.log(req.params.id);
+    //         fetch(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=4f2d4313669b932746a7cbe1b3fff187`)
+           
+    //         .then(res => res.json())
+    //         .then(json => {
+    //             // console.log(json)
+    //             res.render("moviesdetails", { single: json })
+    //         })
+    //       .catch(err=>console.log(err))
+    //       })
+
+        //   favourites
+    app.get('/addfavourites/:id',(req,res)=>{
+        fetch(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=4f2d4313669b932746a7cbe1b3fff187&language=en-US`)
+       
+        .then(res=> res.json())
+        .then(json=>{
+            // console.log(json);
+            favouritesData=json
+            console.log(favouritesData);
+            movie.find({id:favouritesData.id})
+            .then(result=>{
+                if(result.length > 0){
+                    res.redirect('/favourites')
+                }else{
+                    const NewmovieItems= new movie({
+                        id: favouritesData.id,
+                        title: favouritesData.title,
+                        overview: favouritesData.overview,
+                        genres: favouritesData.genres,
+                        poster_path: favouritesData.poster_path,
+                        backdrop_path: favouritesData.backdrop_path,
+                        vote_average: favouritesData.vote_average,
+                        popularity: favouritesData.popularity,
+                        original_title: favouritesData.original_title,
+                        release_date: favouritesData.release_date,
+                        status: favouritesData.status
+                    })
+                    NewmovieItems.save()
+                    .then(result=>{
+                        res.redirect('/favourites')
+                    })
+                }
+            })
+
+        })
+    })
     
 
+app.get('favourites',(res,req)=>{
+    movie.find()
+    .then(result => {
+        res.render('favourites', { myMovies: result })
+    })
+    console.log( myMovies)
+    .catch(err => console.log(err))
+
+
+})
